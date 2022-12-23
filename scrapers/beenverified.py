568193fd088024218297190162a2a4ab2011f208
Ayoub ENNASSIRI <ayo.enn@gmail.com>
2022-12-23 17:03:41 +0100

    Using the correct chrome profile

--- scrapers/beenverified.py
+++ scrapers/beenverified.py
@@ -25,6 +25,14 @@ class BeenVerifiedScrapper:
         self.options.add_argument("enable-automation")
         self.options.add_argument("--disable-infobars")
         self.options.add_argument("--disable-dev-shm-usage")
+        chrome_profile_path = os.environ.get(
+            "CHROME_PROFILE_PATH",
+            '/Users/aennassiri/Library/Application Support/Google/Chrome'
+        )
+        self.options.add_argument("user-data-dir=" + chrome_profile_path)
+        self.options.add_argument("--profile-directory=Profile 1")
+
+        # Selenium with
         self.vars = {}
         self.cache = cache
         self.driver = None
@@ -42,25 +50,29 @@ class BeenVerifiedScrapper:
     def login(self):
         self.driver.get("https://www.beenverified.com/")
         # 2 | setWindowSize | 1680x1005 |
-        self.driver.set_window_size(1680, 1005)
+        # self.driver.set_window_size(1680, 1005)
         # 3 | click | css=.nav__utils-btn:nth-child(2) > .nav__utils-link |
-        self.driver.find_element(
-            By.CSS_SELECTOR,
-            ".nav__utils-btn:nth-child(2) > "
-            ".nav__utils-link"
-        ).click()
-        # 4 | type | id=login-email | sam@masfirm.net
-        self.driver.find_element(By.ID, "login-email").send_keys(
-            os.environ.get("BEEN_VERIFIED_EMAIL",
-                           "ayoub.ennassiri@neoinvest.ai")
-        )
-        # 5 | type | id=login-password | Marcus1995!
-        self.driver.find_element(By.ID, "login-password").send_keys(
-            os.environ.get("BEEN_VERIFIED_PASSWORD", "Murex2019!")
-        )
-        # 6 | click on connect
-        self.driver.find_element(By.ID, "submit").click()
-        sleep(30)
+        try:
+            self.driver.find_element(
+                By.CSS_SELECTOR,
+                ".nav__utils-btn:nth-child(2) > "
+                ".nav__utils-link"
+            ).click()
+            # 4 | type | id=login-email | sam@masfirm.net
+            self.driver.find_element(By.ID, "login-email").send_keys(
+                os.environ.get("BEEN_VERIFIED_EMAIL",
+                               "ayoub.ennassiri@neoinvest.ai")
+            )
+            # 5 | type | id=login-password | Marcus1995!
+            self.driver.find_element(By.ID, "login-password").send_keys(
+                os.environ.get("BEEN_VERIFIED_PASSWORD", "Murex2019!")
+            )
+            # 6 | click on connect
+            self.driver.find_element(By.ID, "submit").click()
+            sleep(30)
+        except Exception:
+            logger.warning(
+                f"Continuing with the session as probably the user is already logged in")
 
     @tools.cached(storage=storage.RemotePickleStorage(url=config.remote_upload_url))
     def retrieve_information(self, link):
@@ -106,13 +118,24 @@ class BeenVerifiedScrapper:
         # Focus on the first element:
         attribute_id = results[0].get_attribute("id")
 
+        # Initial window handle
+        window_handles = self.driver.window_handles.copy()
+
         self.driver.find_element(
             By.CSS_SELECTOR, f"#{attribute_id} .btn"
         ).click()
 
-        WebDriverWait(self.driver, 120).until(
+        # Close the previous tabs
+        sleep(5)
+        for w in window_handles:
+            self.driver.switch_to.window(w)
+            self.driver.close()
+
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+
+        WebDriverWait(self.driver, 30).until(
             expected_conditions.presence_of_element_located(
-                (By.CSS_SELECTOR, ".report-header")))
+                (By.CSS_SELECTOR, ".report-header__title")))
         # 12 | click | css=.report_section__label_title |
 
         try:
