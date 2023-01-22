7674cc7e3ee09469836c07f2bba72b3ea0f78a36
Ayoub Ennassiri <ayo.enn@gmail.com>
2023-01-22 15:16:58 -0600

    Correcting an issue with the urls and adding guidelines for the initial setup

--- ticket.sh
+++ ticket.sh
@@ -1,5 +1,5 @@
 cd Projects/Personal/ticket-washer
-export BEEN_VERIFIED_PASSWORD=Mx2022@@@@@
+export BEEN_VERIFIED_PASSWORD=Murex2023@
 export BEEN_VERIFIED_EMAIL="ayoub.ennassiri@neoinvest.ai"
 export GOOGLE_APPLICATION_CREDENTIALS="./configuration/deeprl-326711-2dd88867b5e4.json"
 export CHROME_PROFILE_PATH="/Users/aennassiri/Library/Application Support/Google/Chrome"
