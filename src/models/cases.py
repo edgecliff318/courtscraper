29a36c3b56ffeef9286d93512eef28a28f1a1db6
Ayoub ENNASSIRI <ayo.enn@gmail.com>
2023-02-27 18:53:09 +0100

    Models definition

---
+++ src/models/cases.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, validator
+from typing import Dict, List, Optional, Union
+
+
+class Case(BaseModel):
+    case_id: str
+    state: Optional[str] = None
+    disposition: Optional[str] = None
+    judge: Optional[str] = None
+    date_filed: Optional[str] = None
+    location: Optional[str] = None
+    case_type: Optional[str] = None
+    court_type: Optional[str] = None
+    court_city: Optional[str] = None
+    court_jurisdiction: Optional[str] = None
+    court_phone: Optional[str] = None
+    court_time: Optional[str] = None
+    client_birthdate: Optional[str] = None
+    client_driver_license: Optional[str] = None
+    offense: Optional[str] = None
+    ticket_speed: Optional[str] = None
+    ticket_posted_speed_limit: Optional[str] = None
+    current_date: Optional[str] = None
+    # Image is a binary file
+    image: Optional[bytes] = None
+    charges: Optional[Dict[str, Dict[str, str]]] = None
+    dockets: Optional[List[Dict[str, Union[str, List]]]] = None
+    services: Optional[List[str]] = None
+    headers: Optional[Dict[str, str]] = None
+    parties: Optional[str] = None
+
+    # Ignore parsing errors for now
+    @validator("charges", pre=True, always=True)
+    def set_charges(cls, v):
+        return v or {}
---
+++ src/models/messages.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, validator
+from datetime import datetime
+from typing import Optional
+
+
+class MessageTemplate(BaseModel):
+    template_id: Optional[str] = None
+    template_name: Optional[str] = None
+    template_type: Optional[str] = None
+    template_text: Optional[str] = None
+    template_language: Optional[str] = None
+    media_enabled: Optional[bool] = False
+    creation_date: Optional[datetime] = None
+    last_updated: Optional[datetime] = None
+
+    @validator("last_updated", pre=True, always=True)
+    def set_last_updated_date_now(cls, v):
+        return v or datetime.now()
+
+    @validator("creation_date", pre=True)
+    def set_creation_date_now(cls, v):
+        return v or datetime.now()
+
+
+class Interaction(BaseModel):
+    case_id: str
+    creation_date: Optional[datetime] = None
+    message: Optional[str] = None
+    type: Optional[str] = None
+    phone: Optional[str] = None
+    status: Optional[str] = None
+
+    @validator("creation_date", pre=True)
+    def set_creation_date_now(cls, v):
+        return v or datetime.now()
