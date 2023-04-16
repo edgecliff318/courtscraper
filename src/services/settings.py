8d8fa9f353afeca2adfaab5a99d2b1db93802851
Ayoub ENNASSIRI <ayo.enn@gmail.com>
2023-04-16 18:00:20 +0200

    Adding settings and accounts

---
+++ src/services/settings.py
@@ -0,0 +1,16 @@
+from src.db import db
+from src.models import settings
+
+
+def get_settings(setting_name: str):
+    settings_single = (
+        db.collection("settings").document(setting_name).get().to_dict()
+    )
+    return settings.Settings(**settings_single)
+
+
+def get_account(account_name: str):
+    account_single = (
+        db.collection("accounts").document(account_name).get().to_dict()
+    )
+    return settings.Account(**account_single)
