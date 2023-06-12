965d8c24d154850a7d5dc5138e5223124e9fd507
Ayoub ENNASSIRI <ayo.enn@gmail.com>
2023-06-12 23:41:35 +0200

    Splitting the generate letter function to use it to Zip multiple letters

--- src/services/letters.py
+++ src/services/letters.py
@@ -1,6 +1,8 @@
 import logging
 import os
-from datetime import timedelta
+import shutil
+import zipfile
+from datetime import datetime, timedelta
 
 from pypdf import PdfReader, PdfWriter
 
@@ -13,9 +15,7 @@ logger = logging.Logger(__name__)
 settings = get_settings()
 
 
-def generate_letter(case_id):
-    case = cases.get_single_case(case_id)
-
+def generate_single_letter(case_id, full_name, first_name, address):
     envelope_file = os.path.join(
         settings.ROOT_PATH, "src", "assets", "envelope_fillable.pdf"
     )
@@ -34,16 +34,14 @@ def generate_letter(case_id):
 
     writer_envelope.update_page_form_field_values(
         writer_envelope.pages[0],
-        {
-            "address": f"{case.formatted_party_name}\n{case.formatted_party_address}"
-        },
+        {"address": f"{full_name}\n{address}"},
     )
 
-    if case.first_name is None:
-        case.first_name = ""
+    if first_name is None:
+        first_name = ""
 
     writer_letter.update_page_form_field_values(
-        writer_letter.pages[0], {"text_1bbvr": case.first_name.capitalize()}
+        writer_letter.pages[0], {"text_1bbvr": first_name.capitalize()}
     )
 
     filename_envelope = f"{case_id}_envelope.pdf"
@@ -63,6 +61,21 @@ def generate_letter(case_id):
     with open(filepath_letter, "wb") as output_stream:
         writer_letter.write(output_stream)
 
+    return filepath_envelope, filepath_letter
+
+
+def generate_letter(case_id):
+    case = cases.get_single_case(case_id)
+
+    filepath_envelope, filepath_letter = generate_single_letter(
+        case_id,
+        case.formatted_party_name,
+        case.first_name,
+        case.formatted_party_address,
+    )
+    filename_envelope = f"{case.case_id}_envelope.pdf"
+    filename_letter = f"{case_id}_letter.pdf"
+
     # Upload the file to the bucket
     blob_envelope = bucket.blob(filename_envelope)
     blob_envelope.upload_from_filename(filepath_envelope)
@@ -79,6 +92,79 @@ def generate_letter(case_id):
         expiration=timedelta(seconds=3600)
     )
     media_url_letter = blob_letter.generate_signed_url(
+        expiration=timedelta(seconds=3600)
+    )
+
+    return media_url_envelope, media_url_letter
+
+
+def generate_many_letters(case_ids):
+    cases_list = cases.get_many_cases(case_ids)
+    # Folder name from timestamp
+    folder_name_envelope = (
+        f"envelopes_{datetime.now().strftime('%Y%m%d%H%M%S')}"
+    )
+    folder_path_envelope = settings.DATA_PATH.joinpath(folder_name_envelope)
+    folder_path_envelope.mkdir(parents=True, exist_ok=True)
+
+    folder_name_letter = f"letters_{datetime.now().strftime('%Y%m%d%H%M%S')}"
+    folder_path_letter = settings.DATA_PATH.joinpath(folder_name_letter)
+    folder_path_letter.mkdir(parents=True, exist_ok=True)
+
+    for case in cases_list:
+        filepath_envelope, filepath_letter = generate_single_letter(
+            case.case_id,
+            case.formatted_party_name,
+            case.first_name,
+            case.formatted_party_address,
+        )
+
+        # Move the files to the folder
+        filepath_envelope.rename(
+            folder_path_envelope.joinpath(filepath_envelope.name)
+        )
+
+        filepath_letter.rename(
+            folder_path_letter.joinpath(filepath_letter.name)
+        )
+
+    # Zip the folder
+    filepath_zip_envelope = settings.DATA_PATH.joinpath(
+        f"{folder_name_envelope}.zip"
+    )
+    filepath_zip_letter = settings.DATA_PATH.joinpath(
+        f"{folder_name_letter}.zip"
+    )
+
+    with zipfile.ZipFile(filepath_zip_envelope, "w") as zip_file:
+        for file in folder_path_envelope.iterdir():
+            zip_file.write(file, file.name)
+
+    with zipfile.ZipFile(filepath_zip_letter, "w") as zip_file:
+        for file in folder_path_letter.iterdir():
+            zip_file.write(file, file.name)
+
+    # Upload the zip file to the bucket
+    blob_zip_envelope = bucket.blob(filepath_zip_envelope.name)
+    blob_zip_envelope.upload_from_filename(filepath_zip_envelope)
+
+    blob_zip_letter = bucket.blob(filepath_zip_letter.name)
+    blob_zip_letter.upload_from_filename(filepath_zip_letter)
+
+    # Delete the folder
+    shutil.rmtree(folder_path_envelope)
+    shutil.rmtree(folder_path_letter)
+
+    # Delete the zip file
+    filepath_zip_envelope.unlink()
+    filepath_zip_letter.unlink()
+
+    # Get the signed url
+    media_url_envelope = blob_zip_envelope.generate_signed_url(
+        expiration=timedelta(seconds=3600)
+    )
+
+    media_url_letter = blob_zip_letter.generate_signed_url(
         expiration=timedelta(seconds=3600)
     )
 
