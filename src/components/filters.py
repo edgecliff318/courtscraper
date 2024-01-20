d1ff5e00e75a84c5795b8a5aa2c9cddf51b818d6
Yassine Elazrak <yassineazrak2019@gmail.com>
2024-01-20 14:50:54 +0100

    Fix margin btns

--- src/components/filters.py
+++ src/components/filters.py
@@ -63,10 +63,10 @@ leads_controls = dbc.Row(
 )
 
 
-def create_date_form_group():
+def create_date_form_group(id: str):
     return generate_form_group(
         label="Date",
-        id="stats-date-selector",
+        id=id,
         placeholder="Select a Date",
         type="DateRangePicker",
         persistence_type="session",
@@ -113,11 +113,11 @@ def create_scrapper_form_group():
 
 def create_button(label, id, width, lg, xs):
     return dbc.Col(
-        dmc.Button(label, id=id, color="dark", size="sm", className="mt-auto"),
+        dmc.Button(label, id=id, color="dark", size="sm", className="mt-auto "),
         width=width,
         lg=lg,
         xs=xs,
-        className="d-flex",
+        className="d-flex mx-2",
     )
 
 
@@ -148,7 +148,7 @@ def create_switch():
 
 monitoring_controls = dbc.Row(
     [
-        dbc.Col(create_date_form_group(), width=3, xs=12, lg=3),
+        dbc.Col(create_date_form_group("monitoring-date-selector"), width=3, xs=12, lg=3),
         dbc.Col(create_interaction_form_group(), width=2, xs=12, lg=2),
         create_button("Monitoring", "monitoring-button", 1, 1, 6),
         create_button("Refresh", "monitoring-refresh-button", 1, 1, 6),
@@ -167,7 +167,7 @@ monitoring_controls = dbc.Row(
 
 stats_controls = dbc.Row(
     [
-        dbc.Col(create_date_form_group(), width=3, xs=12, lg=3),
+        dbc.Col(create_date_form_group("stats-date-selector"), width=3, xs=12, lg=3),
         create_button("Run", "stats-refresh-button", 1, 1, 6),
       
     ],
