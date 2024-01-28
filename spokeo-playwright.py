d56f39867e3f7e3fb41f347a9081b76137044c00
Yassine Elazrak <yassineazrak2019@gmail.com>
2024-01-28 13:58:41 +0100

    fix login spokeo

--- spokeo-playwright.py
+++ spokeo-playwright.py
@@ -3,24 +3,11 @@ from playwright_stealth import stealth_sync , stealth_async
 
 
 # extract info from the page of a single person
-def extract_info(page, link):
-    page.goto(link)
-    page.wait_for_load_state()
-    # get name
-    name = page.query_selector('.e1ndw42t0').inner_text()
-    # get age
-    age = page.query_selector('.e1ndw42t1').inner_text()
-    # get location
-    location = page.query_selector('.e1ndw42t2').inner_text()
-    # get phone number
-    phone_number = page.query_selector('.e1ndw42t3').inner_text()
-    # get email
-    return {
-        "name": name,
-        "age": age,
-        "location": location,
-        "phone_number": phone_number
-    }
+def extract_info(page):
+    container = page.query_selector('.e1usz6i03')
+    phone_number = container.query_selector('#phone-number-content').inner_text()
+    
+    return phone_number
     
 
     
@@ -29,17 +16,22 @@ def extract_info(page, link):
 #search_person("Samama", "Mahmud", token)
 def search_person(page, firstName, lastName, middleName="", city=None, state=None):
     page.goto("https://www.spokeo.com/")
-    q = f"{firstName} {middleName} {lastName}"
-    page.fill('input[name="q"]', q)
-    page.click('button:has-text("Search Now")')
-    page.wait_for_load_state()
-    # select all .e1ndw42t0
+    # q = f"{firstName} {middleName} {lastName}"
+    firstName = "Samama"
+    lastName = "Mahmud"
+    middleName = "B"
+    # https://www.spokeo.com/Samama-Mahmud?middle_name=B
+    url = f"https://www.spokeo.com/{firstName}-{lastName}?middle_name={middleName}"
+    page.goto(url)
+    # https://www.spokeo.com/Samama-Mahmud/Wisconsin/Milwaukee/p2015111502099087352946730813837
+    
     link_elements = page.query_selector_all('.e1ndw42t0')
     links = [
         link_element.get_attribute('href')
         for link_element in link_elements
     ]
     return links
+
 
 
     
@@ -52,12 +44,20 @@ def run(playwright):
     password = "?XCcFLCrhr3uQg@"
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
-    stealth_sync(page)
     page.goto('https://www.spokeo.com/login')
-    page.wait_for_load_state("load")
 
-    page.fill('input[name="email"]', email)
+    #clcik inside the input box
+    page.click('input[name="password"]')
     page.fill('input[name="password"]', password)
+    #clcik inside the input box
+    page.click('input[name="email"]')
+    page.fill('input[name="email"]', email)
+    
+    
+    #disable_stealth(page)
+    stealth_sync(page)
+    page.wait_for_selector('button:has-text("LOGIN")', state="visible")
+
     page.click('button:has-text("LOGIN")')
     page.wait_for_load_state()
     search_person(
