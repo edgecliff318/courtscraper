ac6798396efcd3f1274bc51f23a95764724795a3
Yassine Elazrak <yassineazrak2019@gmail.com>
2024-02-25 22:35:53 +0100

    Add participants selected output to case details

--- src/callbacks/manage/details.py
+++ src/callbacks/manage/details.py
@@ -4,6 +4,7 @@ import logging
 import dash
 import dash_mantine_components as dmc
 from dash import Input, Output, callback, dcc
+from dash_iconify import DashIconify
 
 
 import src.services.cases as cases_service
@@ -150,17 +151,42 @@ def edit_component(refresh, insert, case_id):
 
 @callback(
     Output("case-details-output", "children", allow_duplicate=True),
+    Output("case-details-participants-selected-output", "children"),
     Input("case-details-id", "data"),
-    Input("case-manage-participants", "value"),
+    Input("case-manage-participants", "value"),    
     prevent_initial_call=True,
 )
 def update_participants(case_id, participants):
+    
+    participants_service = ParticipantsService()
+
+    participants_all = participants_service.get_items()
+    participants_selected = []
+    if participants is not None:
+        participants_selected = [
+            dcc.Link(
+                dmc.Button(
+                    f"{p.role.capitalize()} - {p.first_name} {p.last_name}",
+                    color="dark",
+                    variant="light",
+                    size="xs",
+                    rightIcon=DashIconify(icon="carbon:view"),
+                ),
+                href=f"/manage/participants/{p.id}",
+            )
+            for p in participants_all
+            if p.id in participants
+        ]
+    participants_selected = dmc.Group(participants_selected)
+    
     if participants is None:
-        return dash.no_update
+        return dash.no_update , participants_selected
 
     cases_service.patch_case(
         case_id=case_id, data={"participants": participants}
     )
+
+
 
     return dmc.Alert(
         "Participants updated",
@@ -168,4 +194,4 @@ def update_participants(case_id, participants):
         variant="light",
         withCloseButton=True,
         duration=3000,
-    )
+    ), participants_selected
