f0f4ea17f377d041613e67ae3493313f97ff6056
Yassine Elazrak <yassineazrak2019@gmail.com>
2024-04-01 13:58:18 +0000

    Update dependencies and add Azure OpenAI notebook

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ INSTALL_REQUIRES = [
     "google-api-python-client",
     "google-auth-httplib2",
     "google-auth-oauthlib",
-    "langchain",
+    # "langchain",
     "openai",
     "stripe"
 ]
