c5d2f2b6f3fb1f9610f4282d8dcf0a74646353df
Ayoub ENNASSIRI <ayo.enn@gmail.com>
2024-04-11 16:32:23 +0200

    Fixing the actions page

--- src/callbacks/manage/actions.py
+++ src/callbacks/manage/actions.py
@@ -1,17 +1,15 @@
 import logging
 from datetime import datetime
-import pandas as pd
 
 import dash_mantine_components as dmc
+import pandas as pd
 import pytz
 from dash import Input, Output, callback, html
 
 from src.components.cases.status import case_statuses, get_case_status_color
 from src.core.config import get_settings
-from src.services import cases
-from datetime import datetime
 from src.core.dynamic_fields import CaseDynamicFields
-
+from src.services import cases
 
 logger = logging.Logger(__name__)
 
@@ -28,43 +26,82 @@ def convert_date_format(date_str_or_obj, timezone="Etc/GMT-1") -> str:
     date_obj = date_obj.astimezone(tz)
 
     formatted_date = date_obj.strftime("%B %d, %Y")
-    formatted_date = (
-        formatted_date[:-2] + ":" + formatted_date[-2:]
-    )  # Convert +0100 to +01:00
 
     return formatted_date
 
 
 def create_case_card(case_data: dict):
-    location_name = f"{case_data.get('location')} Court of {case_data.get('city')}"
+    location_name = (
+        f"{case_data.get('location')} Court of {case_data.get('city')}"
+    )
     date_str_or_obj_time = case_data.get("court_time", "")
     date_str_or_obj = case_data.get("court_date", "")
-    case_date = "not available"
+    case_date = "No Court Date"
     if date_str_or_obj:
         date_obj = datetime.strptime(date_str_or_obj, "%m/%d/%Y")
-        case_date = f"{convert_date_format(date_obj)} at {date_str_or_obj_time}"
+        case_date = (
+            f"{convert_date_format(date_obj)} at {date_str_or_obj_time}"
+        )
+        # Red if in less than 7 days from now
+        # Orange if in less than 30 days from now
+        # Green if in more than 30 days from now
+        court_date_color = "green"
+
+        if (date_obj - datetime.now()).days < 7:
+            court_date_color = "red"
+        elif (date_obj - datetime.now()).days < 30:
+            court_date_color = "orange"
+    else:
+        court_date_color = "red"
 
     charges_description = case_data.get("charges_description", "")
     case_id = case_data.get("case_id", "N/A")
     status = case_data.get("status") or "filed"
-    full_name = f'{case_data.get("first_name", "")} {case_data.get("last_name", "")}'
+    full_name = (
+        f'{case_data.get("first_name", "")} {case_data.get("last_name", "")}'
+    )
 
     card_layout = [
         dmc.Group(
             [
                 dmc.Text(f"Case#{case_id}", weight=500),
                 dmc.Badge(
-                    case_statuses.get(status, {}).get("short_description", status),
+                    case_statuses.get(status, {}).get(
+                        "short_description", status
+                    ),
                     color=get_case_status_color(status),
                     variant="light",
                 ),
             ],
             position="apart",
+            spacing="xs",
         ),
-        dmc.Text(f"User name: {full_name.lower().capitalize()}", size="sm", color="dimmed"),
-        dmc.Text(f"Court Date: {case_date.lower().capitalize()}", size="sm", color="dimmed"),
-        dmc.Text(f"Court Location: {location_name.lower().capitalize()}", size="sm", color="dimmed"),
-        dmc.Text(f"charges : {charges_description.lower().capitalize()}", size="sm", color="dimmed"),
+        dmc.Text(
+            f"{full_name.lower().capitalize()}",
+            size="sm",
+            color="dark",
+        ),
+        dmc.Group(
+            [
+                dmc.Text(
+                    f"{case_date.lower().capitalize()}",
+                    size="sm",
+                    color=court_date_color,
+                ),
+                dmc.Text(
+                    f"{location_name.lower().capitalize()}",
+                    size="sm",
+                    color="dimmed",
+                ),
+            ],
+            position="apart",
+            spacing="xs",
+        ),
+        dmc.Text(
+            f"{charges_description.lower().capitalize()}",
+            size="sm",
+            color="dark",
+        ),
     ]
 
     return html.A(
@@ -85,7 +122,9 @@ def create_case_column(cases, title):
         dmc.Navbar(
             p="md",
             children=[
-                html.H4(title, style={"marginTop": "4px", "textAlign": "center"}),
+                html.H4(
+                    title, style={"marginTop": "4px", "textAlign": "center"}
+                ),
                 dmc.Divider(size="sm", style={"marginBottom": "10px"}),
                 html.Div(
                     [create_case_card(case) for case in cases],
@@ -106,7 +145,7 @@ def create_case_column(cases, title):
 #     ## should be sorted by court_date
 #     # from src.models import cases
 #     # case_data = CaseDynamicFields().update(cases.Case(**case_details), case_details)
-    
+
 #     cases = [
 #         CaseDynamicFields().update(case, case.model_dump()) for case in cases
 #     ]
@@ -119,34 +158,36 @@ def create_case_column(cases, title):
 #     )
 
 
-
-
 def parse_date_time(case):
-    date_str = case.get('court_date', '01/01/1900') or '01/01/1900'
-    time_str = case.get('court_time', '12:00 AM') or '12:00 AM'
-    sort_date_str = f'{date_str} {time_str}'
+    date_str = case.get("court_date", "01/01/1900") or "01/01/1900"
+    time_str = case.get("court_time", "12:00 AM") or "12:00 AM"
+    sort_date_str = f"{date_str} {time_str}"
     try:
-        return datetime.strptime(sort_date_str, '%m/%d/%Y %I:%M %p')
+        return datetime.strptime(sort_date_str, "%m/%d/%Y %I:%M %p")
     except ValueError:
-        return datetime.strptime('01/01/1900 12:00 AM', '%m/%d/%Y %I:%M %p')
+        return datetime.strptime("01/01/1900 12:00 AM", "%m/%d/%Y %I:%M %p")
+
 
 def create_case_div(cases):
     updated_cases = []
-  
+
     for case in cases:
         case = CaseDynamicFields().update(case, case.model_dump())
-        case['sort_date'] = parse_date_time(case)
+        case["sort_date"] = parse_date_time(case)
         updated_cases.append(case)
-        
+
     df = pd.DataFrame(updated_cases)
-    df['sort_date'] = pd.to_datetime(df['sort_date'])
-    df = df.sort_values('sort_date', ascending=False)
-    updated_cases = df.to_dict('records')
-    
+    if df.empty:
+        return html.Div(style={"overflowY": "auto"})
+    df["sort_date"] = pd.to_datetime(df["sort_date"])
+    df = df.sort_values("sort_date", ascending=True)
+    updated_cases = df.to_dict("records")
+
     case_cards = [create_case_card(case) for case in updated_cases]
 
     return html.Div(case_cards, style={"overflowY": "auto"})
-    
+
+
 @callback(
     Output("case_card_col_todo", "children"),
     Input("court-selector", "value"),
