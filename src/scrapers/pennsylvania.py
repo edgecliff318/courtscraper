4707ce6f6430267248a4f579360a08eb0bb8bcdc
Yassine Elazrak <yassineazrak2019@gmail.com>
2024-05-12 15:00:42 +0200

    Delete case-status.py and update Pennsylvania scraper.- Update Pennsylvania scraper for PDF processing

--- src/scrapers/pennsylvania.py
+++ src/scrapers/pennsylvania.py
@@ -1,14 +1,16 @@
 import asyncio
 import os
-import os.path
 import re
 import sys
 import uuid
+from pathlib import Path
 
 from playwright.async_api import async_playwright
 from rich.console import Console
 
+from src.models import cases as cases_model
 from src.scrapers.base import ScraperBase
+from src.services import cases, leads
 
 sys.path.append(
     os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir) + "/libraries"
@@ -21,155 +23,191 @@ console = Console()
 TIMEOUT = 120000
 WAIT_TIMEOUT = 7000
 PDF_EXTENSION = ".pdf"
-# regex
-CASE_ID_REGEX = r"Docket Number: (MJ-\d+-TR-\d+-(\d+))"
-FIRST_NAME_REGEX = r"Name:----.*?, (.*?)----"
-LAST_NAME_REGEX = r"Name:----(.*?),"
-CITY_REGEX = r"Address\(es\):----Home (.*?),"
-STATE_REGEX = r"Address\(es\):----Home .*?, (.*?)\s"
-CHARGES_DESCRIPTION_REGEX = r"Grade Description S----(.*?)----"
-DATE_OF_BIRTH_REGEX = r"Date of Birth:----(.*?)----"
-ADDRESS_REGEX = r"Address\(es\):----Home (.*?)----"
-CASE_DATE_REGEX = r"Offense Dt\. (.*?)----"
+REGEX_PATTERNS = {
+    "case_id": r"Docket Number: (MJ-\d+-TR-\d+-(\d+))",
+    "first_name": r"Name:----.*?, (.*?)----",
+    "last_name": r"Name:----(.*?),",
+    "city": r"Address\(es\):----Home (.*?),",
+    "state": r"Address\(es\):----Home .*?, (.*?)\s",
+    "charges": r"Grade Description S----(.*?)----",
+    "dob": r"Date of Birth:----(.*?)----",
+    "address": r"Address\(es\):----Home (.*?)----",
+    "case_date": r"Offense Dt\. (.*?)----",
+}
 
 
-class Pennsylvania(ScraperBase):
+class PennsylvaniaScraper(ScraperBase):
     BASE_URL = "https://ujsportal.pacourts.us/CaseSearch"
 
     def __init__(self, start_date, end_date):
         self.start_date = start_date
         self.end_date = end_date
 
-    async def navigate_to_page(self, page):
-        await page.goto(self.BASE_URL, timeout=TIMEOUT)
-        console.log(f"Navigated to page {self.BASE_URL} successfully")
-
-    async def perform_search(self, page, start_date, end_date):
-        console.log(f"Performing search for {start_date} to {end_date}")
-        await page.locator('select[title="Search By"]').select_option(
-            label="Date Filed"
-        )
-        await page.fill('input[name="FiledStartDate"]', start_date)
-        await page.fill('input[name="FiledEndDate"]', end_date)
-        await page.locator("#btnSearch").click()
-        await page.wait_for_timeout(WAIT_TIMEOUT)
-
-    async def generate_case_file(self, page, url: str) -> str | None:
+    async def navigate_to_base_page(self, page):
         try:
-            file_uuid = str(uuid.uuid4())
-            name_file = f"{file_uuid}{PDF_EXTENSION}"
+            await page.goto(self.BASE_URL, timeout=TIMEOUT)
+            console.log(f"Successfully navigated to {self.BASE_URL}")
+        except Exception as e:
+            console.print(
+                f"An error occurred while navigating to {self.BASE_URL}: {str(e)}"
+            )
+
+    async def perform_search_by_date(self, page):
+        try:
+            console.log(f"Performing search from {self.start_date} to {self.end_date}")
+            await page.locator('select[title="Search By"]').select_option(
+                label="Date Filed"
+            )
+            await page.fill('input[name="FiledStartDate"]', self.start_date)
+            await page.fill('input[name="FiledEndDate"]', self.end_date)
+            await page.locator("#btnSearch").click()
+            await page.wait_for_timeout(WAIT_TIMEOUT)
+        except Exception as e:
+            console.print(f"An error occurred while performing the search: {str(e)}")
+
+    async def download_case_file(self, page, url):
+        try:
+            file_path = f"{str(uuid.uuid4())}{PDF_EXTENSION}"
             response = await page.request.get(url)
             if response.status == 200:
-                content = await response.body()
-                with open(name_file, "wb") as file:
-                    file.write(content)
-                console.log("File created successfully")
-            else:
-                console.log("Failed to create file")
+                console.log(f"File {file_path} created successfully")
+                Path(file_path).write_bytes(await response.body())
+                return file_path
 
-            return name_file
+            else:
+                console.print(
+                    f"Failed to fetch the case file. Status code: {response.status}"
+                )
         except Exception as e:
-            console.log(f"An error occurred while creating the file: {str(e)}")
+            console.print(
+                f"An error occurred while downloading the case file: {str(e)}"
+            )
             return None
 
-    async def load_case_file(self, file_name: str):
+    def parse_case_details(self, content):
+        details = {}
+        try:
+            for key, pattern in REGEX_PATTERNS.items():
+                match = re.search(pattern, content)
+                details[key] = match.group(1) if match else ""
+        except Exception as e:
+            console.print(f"Error occurred while parsing case details: {str(e)}")
+        return details
+
+    async def load_pdf_content(self, pdf_file_name: str):
         from unstructured.partition.pdf import partition_pdf
 
         try:
-            elements = partition_pdf(file_name)
-            content = "----".join([element.text for element in elements])
-            return content
+            pdf_elements = partition_pdf(pdf_file_name)
+            combined_content = "----".join([element.text for element in pdf_elements])
+            return combined_content
         except FileNotFoundError:
-            console.log(f"File {file_name} not found.")
+            console.log(f"PDF file {pdf_file_name} not found.")
             return None
 
-    async def fetch_case_urls(self, row_cells):
-        case_urls = []
+    async def process_case_rows(self, page):
         try:
-            for cell in row_cells:
-                url = await cell.eval_on_selector_all(
+            rows = await page.query_selector_all("#caseSearchResultGrid tr")
+            for row in rows:
+                case_url = await row.eval_on_selector_all(
                     "a.icon-wrapper",
-                    "elements => elements.map(element => element.href)",
+                    "elements => elements.map(element => element.href ? element.href : null)",
                 )
-                if url:
-                    case_urls.append(url[0])
-        except Exception as e:
-            print(f"An error occurred while fetching case URLs: {e}")
-        return case_urls
+                console.log(f"Case URL: {case_url}")
+                if isinstance(case_url, list):
+                    if len(case_url) == 0:
+                        continue
+                    case_url = case_url[0]
 
-    def extract_with_pattern(self, pattern, input_text, default_value=""):
-        try:
-            matched_pattern = re.search(pattern, input_text)
-            return matched_pattern.group(1) if matched_pattern else default_value
-        except Exception as e:
-            print(f"An error occurred while extracting with pattern: {e}")
-            return default_value
-
-    def extract_case_details(self, case_text):
-        try:
-            case_details = {
-                "case_id": self.extract_with_pattern(CASE_ID_REGEX, case_text),
-                "first_name": self.extract_with_pattern(FIRST_NAME_REGEX, case_text),
-                "last_name": self.extract_with_pattern(LAST_NAME_REGEX, case_text),
-                "city": self.extract_with_pattern(CITY_REGEX, case_text),
-                "state": self.extract_with_pattern(STATE_REGEX, case_text),
-                "charges_description": self.extract_with_pattern(
-                    CHARGES_DESCRIPTION_REGEX, case_text
-                ),
-                "date_of_birth": self.extract_with_pattern(
-                    DATE_OF_BIRTH_REGEX, case_text
-                ),
-                "address": self.extract_with_pattern(ADDRESS_REGEX, case_text),
-                "case_date": self.extract_with_pattern(CASE_DATE_REGEX, case_text),
-            }
-
-            return case_details
-        except Exception as e:
-            print(f"An error occurred while extracting case details: {e}")
-            return None
-
-    async def extract_table_data(self, page):
-        table = await page.query_selector("#caseSearchResultGrid")
-        rows = await table.query_selector_all("tr")
-        return rows
-
-    async def process_case_data(self, webpage, case_rows):
-        case_urls = await self.fetch_case_urls(case_rows)
-        console.log(f"Case URLs length: {len(case_urls)}")
-        cases = []
-        try:
-            for case_url in case_urls:
-                case_file = await self.generate_case_file(webpage, case_url)
+                case_file_path = await self.download_case_file(page, case_url)
                 try:
-                    case_content = await self.load_case_file(case_file)
-                    case_data = self.extract_case_details(case_content)
-                except Exception as e:
-                    console.log(f"An error occurred while extracting case details: {str(e)}")
-                    case_data = {}
-                print(case_data)
-                cases.append(case_data)
-                
-            return cases
-        except Exception as e:
-            console.log(f"An error occurred while processing case data: {str(e)}")
+                    if case_file_path:
+                        content = await self.load_pdf_content(case_file_path)
+                        if content:
+                            case_details = self.parse_case_details(content)
+                            console.log(case_details)
+                            case_details = cases_model.Case.model_validate(case_details)
+                            cases.insert_case(case_details)
+                            leads.insert_lead_from_case(case_details)
+                            console.log(f"inseted case {case_details.case_id}")
 
-    async def main(self):
-        try:
-            async with async_playwright() as p:
-                browser = await p.chromium.launch(headless=True)
-                page = await browser.new_page()
-                console.log("Browser opened")
-                await self.navigate_to_page(page)
-                await self.perform_search(page, self.start_date, self.end_date)
-                rows = await self.extract_table_data(page)
-                console.log(f"Rows length: {len(rows)}")
-                await self.process_case_data(page, rows)
-                console.log("Process completed")
-                await browser.close()
+                except Exception as e:
+                    console.print(
+                        f"An error occurred while processing case rows: {str(e)}"
+                    )
+                finally:
+                    try:
+                        if os.path.exists(case_file_path):
+                            os.remove(case_file_path)
+                            console.log(f"File {case_file_path} removed successfully")
+                    except Exception as e:
+                        console.print(
+                            f"An error occurred while removing file: {str(e)}",
+                            style="bold red",
+                        )
+
         except Exception as e:
-            console.log(f"An error occurred: {str(e)}")
+            console.print(f"An error occurred while processing case rows: {str(e)}")
+
+    async def run_main_process(self, pw):
+        browser = None
+        try:
+            console.log("Launching browser...")
+            browser = await pw.chromium.launch(headless=True)
+            page = await browser.new_page()
+            console.log("Browser launched successfully.")
+
+            console.log("Navigating to page...")
+            await self.navigate_to_base_page(page)
+            console.log("Navigation to page completed.")
+
+            console.log("Performing search...")
+            await self.perform_search_by_date(page)
+            console.log("Search completed.")
+
+            console.log("Processing cases...")
+            await self.process_case_rows(page)
+            console.log("Case processing completed.")
+
+            console.log("Process completed successfully.")
+        except Exception as e:
+            console.print(f"An error occurred during the main process: {str(e)}")
+        finally:
+            if browser:
+                console.log("Closing browser...")
+                await browser.close()
+                console.log("Browser closed successfully.")
+
+    async def execute_main_process(self):
+        try:
+            async with async_playwright() as playwright:
+                console.log("Starting main process...")
+                await self.run_main_process(playwright)
+                console.log("Main process completed successfully.")
+        except Exception as e:
+            console.print(
+                f"An error occurred during the execution of the main process: {str(e)}"
+            )
+
+    def run(self) -> None:
+        try:
+            console.log("Starting the scraper...")
+            asyncio.run(self.execute_main_process())
+            console.log("Scraper completed successfully.")
+        except Exception as e:
+            console.print(f"An error occurred while running the scraper: {str(e)}")
 
 
 if __name__ == "__main__":
-    scraper = Pennsylvania(start_date="2024-02-20", end_date="2024-02-24")
-    asyncio.run(scraper.main())
+    try:
+        console.log("Initializing Pennsylvania Scraper...")
+        scraper = PennsylvaniaScraper(start_date="2024-02-20", end_date="2024-02-24")
+        console.log("Pennsylvania Scraper initialized successfully.")
+
+        console.log("Running Pennsylvania Scraper...")
+        scraper.run()
+        console.log("Pennsylvania Scraper run completed successfully.")
+    except Exception as e:
+        console.print(
+            f"An error occurred while running the Pennsylvania Scraper: {str(e)}"
+        )
