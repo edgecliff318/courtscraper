575ec8aa148094a7d04c7d391debd05bdd929700
ai-general <whitesnowman113@gmail.com>
2024-07-15 13:51:31 +0000

    update arkansas

--- src/scrapers/arkansas.py
+++ src/scrapers/arkansas.py
@@ -4,18 +4,16 @@ import os
 import requests
 import pandas as pd
 
-from models.cases import Case
-from models.leads import Lead
 from dotenv import load_dotenv
-from src.scrapers.base import ScraperBase
 from datetime import datetime, timedelta
 from tempfile import NamedTemporaryFile
 from rich.console import Console
 from rich.progress import Progress
 from twocaptcha import TwoCaptcha
 
-# Load environment variables
-load_dotenv()
+from models.cases import Case
+from models.leads import Lead
+from src.scrapers.base.scraper_base import ScraperBase
 
 # Get API key from environment variables
 TWOCAPTCHA_API_KEY = os.getenv('TWOCAPTCHA_API_KEY')
@@ -39,18 +37,24 @@ class ArkansasScraper(ScraperBase):
     }
 
     def split_full_name(self, name):
-        """ Split a full name into first, middle, and last names. """
-        parts = re.split(r'[\s,\-\.]+', name)
-        first_name = middle_name = last_name = ''
+        # Prepare variables for first, middle, and last names
+        first_name = middle_name = last_name = ""
 
-        if len(parts) > 2:
-            first_name = parts[0]
-            middle_name = ' '.join(parts[1:-1])
-            last_name = parts[-1]
-        elif len(parts) == 2:
-            first_name, last_name = parts
-        elif len(parts) == 1:
-            first_name = parts[0]
+        # Use regular expression to split on space, comma, hyphen, or period.
+        parts = re.split(r"[,]+", name)
+        if len(parts) > 1:
+            last_name = parts[0]
+
+            # Remove the first space from the second part
+            second_part = parts[1].lstrip()
+            second_part = re.split(r"[\s]+", second_part)
+
+            if len(second_part) > 1:
+                first_name = second_part[0]
+                middle_name = second_part[1]
+
+            else:
+                first_name = second_part[0]
 
         return first_name, middle_name, last_name
 
@@ -183,6 +187,7 @@ class ArkansasScraper(ScraperBase):
             if filing_date:
                 filing_date = datetime.strptime(filing_date, "%Y-%m-%dT%H:%M:%S.%fZ")
             case_dict["filing_date"] = filing_date
+            case_dict["case_date"] = filing_date
 
             if parties:
                 first_name, middle_name, last_name = self.split_full_name(parties[0].get("name"))
@@ -192,53 +197,70 @@ class ArkansasScraper(ScraperBase):
                     "last_name": last_name,
                 })
 
+            status = case_dict.get("status")
+            if status:
+                case_dict["status"] = "new"
+
+            case_dict.update({
+                "court_code": case_dict.get("court_id"),
+                "state": "AR",
+                "source": "arkansas_state",
+            })
             case_dicts.append(case_dict)
         return case_dicts
     
     def scrape(self):
         """ Main scraping function to handle the entire scraping process. """
         last_filing_date = self.state.get("last_filing_date", "2024-05-01")
-        
-        try:
-            filing_date = last_filing_date
-            not_found_count = 0
-            cases = []
-
-            while True:
+        filing_date = last_filing_date
+        not_found_count = 0
+        while True:
+            try:
+                cases = []
                 if not_found_count > 10:
                     console.log("Too many filing dates not found. Ending the search.")
                     break
 
+                filing_date = self.increase_date_by_one_day(filing_date)
                 case_data = self.scrape_cases_for_filing_date(filing_date)
 
                 if case_data is None:
                     not_found_count += 1
-                else:
-                    not_found_count = 0
-                    cases.extend(case_data)
+                    continue
 
-                filing_date = self.increase_date_by_one_day(filing_date)
-                last_filing_date = filing_date
-                console.log(f"last_filing_date-{last_filing_date}")
-                self.state["last_filing_date"] = filing_date
+                not_found_count = 0
+                console.log(
+                    f"Downloading case details for {last_filing_date}"
+                )
+                cases.extend(case_data)
+                          
+                console.log(f"Total {len(cases)} cases")
 
-                # Save the state, uncomment the next line if state persistence is required
-                self.update_state()
-                
-            console.log(f"Total {len(cases)} cases")
-
-            case_dicts = self.process_cases(cases)        
-            for case_dict in case_dicts:
-                case_id = case_dict["case_id"]
-                if self.check_if_exists(case_id):
-                    console.log(f"Case {case_id} already exists. Skipping...")
-                else:
+                case_dicts = self.process_cases(cases)        
+                for case_dict in case_dicts:
+                    case_id = case_dict["case_id"]
+                    if self.check_if_exists(case_id):
+                        console.log(
+                            f"Case {case_id} already exists. Skipping..."
+                        )
+                        continue
+                    console.log(f"case_dict-{case_dict}")
                     console.log(f"Inserting case {case_id}...")
                     self.insert_case(case_dict)
+                    console.log(
+                        f"Inserted case for {case_id})"
+                    )
                     self.insert_lead(case_dict)
-    
-        except Exception as e:
-            console.log(f"Error occurred while scraping: {e}")
+                    console.log(
+                        f"Inserted lead for {case_id}"
+                    )
+                
+                self.state["last_filing_date"] = filing_date
+                # self.update_state()
+            except Exception as e:
+                console.log(f"Failed to insert case - {e}")
+                continue
+
     
 if __name__ == "__main__":
     arkansasscraper = ArkansasScraper()
