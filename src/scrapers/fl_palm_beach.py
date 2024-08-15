d28ae8e6cfa3f0f4c743a6895e328ba34cb2b47c
ai-general <whitesnowman113@gmail.com>
2024-08-15 14:15:11 +0000

    update palm beach

--- src/scrapers/fl_palm_beach.py
+++ src/scrapers/fl_palm_beach.py
@@ -3,7 +3,7 @@
 import re
 import os
 import requests
-import pandas as pdx
+import pandas as pd
 
 from playwright.async_api import async_playwright, TimeoutError
 from urllib.parse import urlparse, parse_qs
@@ -206,8 +206,18 @@ class FLPalmBeachScraper(ScraperBase):
         # await page load
         await self.wait_for_page_load()
 
-        case_id = await self.page.inner_text(f'#cphBody_gvResults_lbCaseNumber_{order}')
-        court_id = await self.get_court_id()
+        case_id = ""
+        try:
+            case_id = await self.page.inner_text(f'#cphBody_gvResults_lbCaseNumber_{order}')
+        except Exception as e:
+            console.log("Error while getting case id: ", e)
+
+        court_id = ""
+        try:
+            court_id = await self.get_court_id()
+        except Exception as e:
+            console.log("Error while getting court id: ", e)
+
         await self.page.click(f'#cphBody_gvResults_lbCaseNumber_{order}', timeout = 6000)
 
         # await page load
@@ -260,7 +270,7 @@ class FLPalmBeachScraper(ScraperBase):
         try:
             filing_date =  await self.get_next_cell_text(self.page, "Filing Date")
             filing_date = (
-                datetime.strptime(filing_date, "%m/%d/%Y").date().strftime("%Y-%m-%d")
+                datetime.strptime(filing_date, "%m/%d/%Y")
                 if filing_date
                 else ""
             )
@@ -315,33 +325,14 @@ class FLPalmBeachScraper(ScraperBase):
             "county": "Palm Beach county",
             "source": "Florida State, Palm Beach county",
         }
-        with Progress() as progress:
-            task = progress.add_task(
-                "[red]Inserting cases...", total=len(case_dict)
-            )
-            
-            case_id = case_dict.get("case_id")
-            if self.check_if_exists(case_id):
-                console.log(
-                    f"Case {case_id} already exists. Skipping..."
-                )
-                progress.update(task, advance=1)
-            else:
-                self.insert_case(case_dict)
-                self.insert_lead(case_dict)
-
-            progress.update(task, advance=1)
         
         return case_dict
     
     async def scrape(self):
-        # to track records to skip duplicates
-        already_processed = []
         await self.init_browser()
 
         court_types = "Criminal Traffic"
         last_offense_begin_date = self.state.get("last_offense_begin_date", "2024-07-22")
-        offense_begin_date = last_offense_begin_date
         not_found_count = 0
         page_no = 1
 
@@ -353,8 +344,9 @@ class FLPalmBeachScraper(ScraperBase):
                         "Too many filing dates not found. Ending the search."
                     )
                     break
-                last_offense_begin_date = self.increase_date_by_one_day(last_offense_begin_date)
+
                 offense_begin_date = last_offense_begin_date
+                last_offense_begin_date = self.increase_date_by_one_day(last_offense_begin_date)
 
                 console.log("last_offense_begin_date", last_offense_begin_date)
                 await self.search_by_case_number(court_types, offense_begin_date)
@@ -363,9 +355,8 @@ class FLPalmBeachScraper(ScraperBase):
                 await self.wait_for_page_load()
 
                 records_per_page = 50
-                # calculate total pages on basis of records per page
-                total_pages_count = int(200 / records_per_page)
-                console.log("total_pages_count", total_pages_count)
+                total_pages_count = 4
+
                 try:
                     # select per page records
                     await self.page.locator("#cphBody_cmbPageSize").select_option(
@@ -375,23 +366,23 @@ class FLPalmBeachScraper(ScraperBase):
                     console.log("Failed to find and select all results. Exiting...")
                     not_found_count += 1
                     continue
-
+                
+                not_found_count = 0
                 page_no = 1
+
                 while True:
                     # loop to traverse pagination
                     console.log(f"Processing Page No:\t{page_no}")
                     order = -1
-                    while order < records_per_page:
+                    while order < (records_per_page - 1):
                         order += 1
                         console.log("order", order)
                         # loop to traverse cases
                         page_order = (page_no, order)
-                        if page_order in already_processed:
-                            console.log(f"Already Processed:\t{page_order}")
-                            order += 1
-                            continue
 
                         console.log(f"Processing:\t{page_order}")
+
+                        case_dict = {}
                         try:
                             case_dict = await self.detail_search(order)
                         except Exception as e:
@@ -399,10 +390,9 @@ class FLPalmBeachScraper(ScraperBase):
                             console.log(
                                 f"Case not found. Skipping ..."
                             )
-                            not_found_count += 1
                             continue
+
                         console.log("case_dict", case_dict)
-                        not_found_count = 0
 
                         case_id = case_dict["case_id"]
                         if self.check_if_exists(case_id):
@@ -411,13 +401,14 @@ class FLPalmBeachScraper(ScraperBase):
                             )
                             await self.page.evaluate("window.history.back()")
                             console.log("Went Back...")
+
                             continue
                     
-                        console.log(f"Inserting case {case_id}...")
+                        console.log(f"Inserting case for {case_id}...")
                         self.insert_case(case_dict)
-                        console.log(f"Inserted case for {case_id})")
+                        console.log(f"Inserting lead for {case_id})")
                         self.insert_lead(case_dict)
-                        console.log(f"Inserted lead for {case_id}")
+                        console.log("Inserted case and lead for ", case_id)
 
                         console.log(f"Order No:\t{order}")
                         await self.page.evaluate("window.history.back()")
@@ -425,9 +416,6 @@ class FLPalmBeachScraper(ScraperBase):
 
                         # await page load
                         await self.wait_for_page_load()
-
-                        already_processed.append(page_order)
-                        console.log("Added to already processed...")
 
                     # page while loop break condition
                     page_no += 1
@@ -443,8 +431,12 @@ class FLPalmBeachScraper(ScraperBase):
                         # break when page reaches last page
                         break
 
-                    self.state["last_offense_begin_date"] = last_offense_begin_date
+                self.state["last_offense_begin_date"] = last_offense_begin_date
+                # If date less than today update
+                if pd.to_datetime(offense_begin_date) < pd.to_datetime("today"):
                     self.update_state()
+                else:
+                    break
 
             except Exception as e:
                 console.log(f"Failed while scraping - {e}")
